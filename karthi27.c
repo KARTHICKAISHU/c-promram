c9955d3991c133124e6edb0a968e8c0f7531ff6a
KARTHICKAISHU <35991674+KARTHICKAISHU@users.noreply.github.com>
2018-02-12 13:49:57 +0530

    Create karthi27.c

---
+++ karthi27.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main ( void )
+{
+int minutes;
+scanf("%d",&minutes);
+if ( (minutes / 60) == 1 )
+printf("%d ",minutes/60);
+else 
+printf("%d ",minutes/60);
+if ( (minutes % 60) == 1) 
+printf("%d",minutes%60);
+else 
+printf("%d",minutes%60);
+getch();
+return 0;
+}
