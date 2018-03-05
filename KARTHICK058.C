911f29ac31f55c2b43c63b12d1a102914d6276a3
KARTHICKAISHU <35991674+KARTHICKAISHU@users.noreply.github.com>
2018-03-05 13:27:52 +0530

    Update KARTHICK058.C

--- KARTHICK058.C
+++ KARTHICK058.C
@@ -2,15 +2,15 @@
 int main(void) 
 {
 int x,y,temp;
-printf("\n enter two numbers to swap:");
+printf("\n enter two numbers to swap :  ");
 scanf("%d %d",&x,&y);
-printf("\n before swapping:");
+printf("\n before swapping : ");
 printf("\nx=%d",x);
 printf("\ny=%d",y);
 temp=x;
 x=y;
 y=temp;
-printf("\n after swapping:");
+printf("\n after swapping : ");
 printf("\nx=%d",x);
 printf("\ny=%d",y);
 return 0;
