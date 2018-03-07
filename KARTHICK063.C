d0d94971f09cab2c641de57e2c518aa4af0f035e
KARTHICKAISHU <35991674+KARTHICKAISHU@users.noreply.github.com>
2018-03-07 14:23:23 +0530

    Create KARTHICK063.C

---
+++ KARTHICK063.C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) 
+{
+	printf("\n ENTER THE STRING : ");
+	char a[100];
+	gets(a);
+	int i,count=1;
+	
+	for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i]==' ')
+		{
+			count=count+1;
+		}
+	}
+	printf("%d",count);
+	return 0;
+}
