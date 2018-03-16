7ac2ef345363f1b8b38732283487910af7c80253
KARTHICKAISHU <35991674+KARTHICKAISHU@users.noreply.github.com>
2018-03-16 16:10:27 +0530

    Create KARTHICK081.C

---
+++ KARTHICK081.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void) 
+{
+	int n1,n2,i;
+	printf("\n :::enter the numbers::::");
+	for(i=1;i<=3;i++)
+	{
+	scanf("%d %d\n",&n1,&n2);
+	if(n1>n2)
+	{
+	printf("\n%d",n1-n2);
+	}
+	else
+	{
+	printf("\n%d",n2-n1);
+	}}
+	return 0;
+}
