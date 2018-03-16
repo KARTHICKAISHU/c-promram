a2479756752fd4fa91d165ee90c4657e0ca52c2a
KARTHICKAISHU <35991674+KARTHICKAISHU@users.noreply.github.com>
2018-03-16 15:58:41 +0530

    Create KARTHICK078.C

---
+++ KARTHICK078.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void) 
+{
+	int n;
+	printf("\n : : ENTER : THE : NUMBERS : :");
+	scanf("%d",&n);
+	if(n%13==0)
+	{
+	printf("\n : YES :");
+	}
+    else
+    {
+    printf("\n : NO :");
+    }
+    return 0;
+    }
