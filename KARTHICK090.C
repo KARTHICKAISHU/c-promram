d2a8dca0934e3a1b28d6b623e1cc7caf9f9a19ac
KARTHICKAISHU <35991674+KARTHICKAISHU@users.noreply.github.com>
2018-03-19 16:13:51 +0530

    Update KARTHICK090.C

--- KARTHICK090.C
+++ KARTHICK090.C
@@ -2,17 +2,16 @@
 #include<string.h>
 int main(void) 
 {
-	char str[50];
-	int i,n;
-	printf("\nenter the alphanumeric string:\n");
-	scanf("%s",str);
-	n=strlen(str);
-	for(i=0;i<n;i++)
-	{
-		if(str[i]>='0' && str[i]<='9')
-		{
-			printf("%c",str[i]);
-		}
-	}
-	return 0;
+char str[50];
+int i,n;
+printf(" ::enter the alphanumeric string::\n");
+scanf("%s",str);
+n=strlen(str);
+for(i=0;i<n;i++)
+{
+if(str[i]>='0' && str[i]<='9')
+{
+printf("%c",str[i]);
+}}
+return 0;
 }
